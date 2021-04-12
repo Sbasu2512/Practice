663d1321af1196e9286c77de978e3e835a5b5b1c
Sayantan  Basu <sayantanbasu@Sayantans-MacBook-Air.local>
2021-04-12 09:54:20 -0400

    lastindexof works with for loop

--- Javascript/lastindexof.js
+++ Javascript/lastindexof.js
@@ -1,19 +1,29 @@
+//you can inverse the array and use indexOf. then subtract the value from the total length of your array
+/* Define a function lastIndexOf.
+
+When this function is given an array and a value, it should return the index of the last time the value occurs in the array. If the value never occurs, the function should return -1.
+
+for example
+
+lastIndexOf([ 5, 5, 5 ], 5); output should be 2
+lastIndexOf([ 0, 1, 4, 1, 2 ], 1); output should be 3 
+
+you can inverse the array and use indexOf. then subtract the value from the total length of your array
+*/
+
 function lastIndexOf(array, value) 
 {
-  let n = array.length ;
-  let i = 0 ;
-  while(i<n){
-    if(array[i] === value){
-      
-      break;
-    }i++;
-    
-  }
-  if(i<n){
+  for(let i=array.length; i>=0; i--){
+    if(array[i] == value){
       return i;
-    }else{
-      return -1;
     }
-
+  }
+  return -1;
 
 }
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1) );
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2));
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3));
+console.log(lastIndexOf([ 5, 5, 5 ], 5));
+console.log(lastIndexOf([], 3));
