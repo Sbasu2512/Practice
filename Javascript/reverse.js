9e3d7edffafbf8c92dcae8d549e50c977eacfb08
Sayantan Basu <sayantanbasu@live.in>
2021-05-25 06:04:58 -0400

    improve

--- Javascript/reverse.js
+++ Javascript/reverse.js
@@ -1,11 +1,12 @@
 const reverse = (arr) => {
+  let newArr;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = arr[i].length; j >= 0; j--) {
+    for (let j = arr[i].length-1; j >= 0; j--) {
       console.log(arr[i][j]);
-      arr = arr.push(arr[i][j]);
+      newArr = arr.push(arr[i]);
     }
   }
-  return arr;
+  return newArr;
 };
 
 reverse(["maharani", "shivanee"]);
